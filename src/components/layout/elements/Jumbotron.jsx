078898c87d29eeb59c8cb0f77dfb8bdb41586eda
rahmahnr15 <nurrahmah382@gmail.com>
2022-04-08 06:18:30 +0700

    update jumbotron

--- src/components/layout/elements/Jumbotron.jsx
+++ src/components/layout/elements/Jumbotron.jsx
@@ -23,9 +23,9 @@ const Jumbotron = () => {
         <div className="max-w-md mx-auto px-3 h-full flex justify-between pt-8 items-center">
             <Searchbar/>
 
-            <div class="relative inline-flex">
-                <svg class="w-3 h-3 absolute top-0 right-0 mr-4 mt-3 pointer-events-none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 412 232"><path d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.764-9.763 25.592 0 35.355l181 181c4.88 4.882 11.279 7.323 17.677 7.323s12.796-2.441 17.678-7.322l181-181c9.763-9.764 9.763-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z" fill="#648299" fill-rule="nonzero"/></svg>
-                <select class="border border-gray-300 text-xs rounded-full text-black h-full py-2 pl-5 pr-8 
+            <div className="relative inline-flex">
+                <svg className="w-3 h-3 absolute top-0 right-0 mr-4 mt-3 pointer-events-none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 412 232"><path d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.764-9.763 25.592 0 35.355l181 181c4.88 4.882 11.279 7.323 17.677 7.323s12.796-2.441 17.678-7.322l181-181c9.763-9.764 9.763-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z" fill="#648299" fillRule="nonzero"/></svg>
+                <select className="border border-gray-300 text-xs rounded-full text-black h-full py-2 pl-5 pr-8 
                 bg-white hover:border-gray-400 hover:text-red focus:outline-none appearance-none">
                     <option>Urutkan</option>
                     <option>Terbaru</option>
